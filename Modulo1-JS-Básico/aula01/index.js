03960d868a4b07f181127c28d860096eff861f36
Luke0195 <lucas201273@gmail.com>
2020-07-19 19:29:52 -0300

    initial commit

---
+++ Modulo1-JS-Básico/aula01/index.js
@@ -0,0 +1,10 @@
+// Aula 01: console.log
+
+// Formas de se usar string
+console.log('Lucas Alves');
+console.log("Lucas Alves");
+console.log(` 'Lucas' "Alves"`); // Usando crassis podemos usar template String 
+
+console.log(1,3,4,5,6); // Numeros inteiros
+
+console.log(3.14); // Números Flutuantes 
