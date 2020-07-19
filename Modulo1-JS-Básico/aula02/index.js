243c431a9b4a56c0e46240b029095e763acce9a4
Luke0195 <lucas201273@gmail.com>
2020-07-19 20:02:30 -0300

    aula2

---
+++ Modulo1-JS-Básico/aula02/index.js
@@ -0,0 +1,17 @@
+// Aula 02: Comentários de código 
+
+
+// Formas de se criar um comentário
+console.log('Olá mundo !'); // Isso é um comentário 
+console.log('Linha1');
+console.log('Linha2');
+//console.log('Linha3');
+console.log('Linha4')
+// O motor do JavaScript será executado 
+
+// Isso é um comentário de linha
+
+
+/** Comentários de bloco
+ * 
+*/
