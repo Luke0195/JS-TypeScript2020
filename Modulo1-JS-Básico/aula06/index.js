9964128e407e130f9421daf5e616016b8e85e6a6
Luke0195 <lucas201273@gmail.com>
2020-07-19 21:48:59 -0300

    aula06

---
+++ Modulo1-JS-Básico/aula06/index.js
@@ -0,0 +1,8 @@
+// Aula06: Let vs Var - Primeira diferença
+// ECMAScript 2015 (ES6)
+nome = 'Luiz' 
+// Variáveis do tipo var podem ser redeclaradas usando a palavra reservada var
+
+// Isso gera uma variável global e isso pode causa problemas durante a aplicação
+// Devemos sempre usar as palavras reservadas let ou const
+console.log(nome);
