18004af7c3be234e0cf1936d62130b0340f61cf3
Luke0195 <lucas201273@gmail.com>
2020-07-20 17:36:10 -0300

    aula09

---
+++ Modulo1-JS-Básico/aula09/js/script.js
@@ -0,0 +1,8 @@
+alert('Esse programa realizar a soma de 2 números');
+
+const numero1 = Number(prompt('Informe o primeiro número'));
+const numero2 = Number(prompt('Informe o segundo número'));
+
+const resultado = numero1 + numero2;
+
+alert(` O resultado da soma dos números ${numero1} e ${numero2} é: ${resultado}`);
