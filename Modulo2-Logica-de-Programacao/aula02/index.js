888e99d0b998bab512acff8672267f4ed69a2d5a
Luke0195 <lucas201273@gmail.com>
2020-07-23 10:52:16 -0300

    aula02

---
+++ Modulo2-Logica-de-Programacao/aula02/index.js
@@ -0,0 +1,11 @@
+// Aula 02: Operadores Lógicos 
+
+/* Operadores Lógicos
+*  && -> and -> e :Todas as expressões precisam  ser verdadeiras para retornar verdadeiro.
+*  || ->  or  -> ou : Todas as expressões precisam ser falsas para retornar falsas.
+*   ! ->  not -> não : Nega o valor da expressão, se ela for verdadeira ela passa a ser falso
+*/
+
+const expressaoAnd=(true && false && true && true); 
+const expressaoOr = false || false;
+console.log(expressaoOr);
