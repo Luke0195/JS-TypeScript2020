673bb21746beafd7e12ca6cfbbe2dc84f5f25d02
Luke0195 <lucas201273@gmail.com>
2020-07-23 11:13:04 -0300

    aula03

---
+++ Modulo2-Logica-de-Programacao/aula03/index.js
@@ -0,0 +1,11 @@
+// Aula 03:  if, else. else(1)
+
+const hora =13;
+
+if(hora >=0 && hora<12){
+  console.log('Bom Dia !');
+}else if( hora>=12  && hora <18){
+  console.log('Boa Tarde');
+}else if(hora>= 18 && hora <=0){
+  console.log('Boa noite');
+}
