424af320f42a2757d017c646efd6c1c0e0ea64ca
Luke0195 <lucas201273@gmail.com>
2020-07-28 13:38:41 -0300

    aula14

---
+++ Modulo2-Logica-de-Programacao/aula14/index.js
@@ -0,0 +1,19 @@
+// Aula 14: Break e continue 
+
+const array = [ 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9];
+
+
+for( valor of array){ // Retorna o valor
+
+
+  if(valor === 2){
+    
+  }
+
+  console.log(valor);
+
+  if(valor === 7){
+    console.log('Achei o numero 7');
+    break;
+  }
+}
